9a9b69c0651eae97a922f38806c32f4d4b6969e8
Ricky T <rickytsui@hotmail.com>
2016-02-21 10:28:04 -0500

    first version of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to avoid costly computation time to find the inverse of a matrix
+## Author: Ricky T
+## Date: 2016-02-21
 
-## Write a short comment describing this function
-
+## Summary: creates a special matrix that contains 4 functions
+## Example: m <- makeCacheMatrix(matrix(1:4,ncol=2,nrow=2))
 makeCacheMatrix <- function(x = matrix()) {
+    
+    ## initialize
+    m <- NULL
+    
+    ## function 1
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
 
+    ## function 2
+    get <- function() x
+    
+    ## function 3
+    setsolve <- function(solve) m <<- solve
+    
+    ## function 4
+    getsolve <- function() m
+    
+    ## return a list of 4 functions
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## Summary: return the inverse of the special matrix makeCacheMatrix
+## Example: cacheSolve(m)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## find if the special matrix exists
+    m <- x$getsolve()
+    
+    ## return cached answer if exists
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    ## compute invserse if does not exist
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    
+    ## Return a matrix that is the inverse of 'x'
+    m
 }
